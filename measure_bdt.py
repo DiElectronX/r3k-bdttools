9433f14b11da6fe25d29e1c1e64e18e6487c4b96
Noah Zipper <noah.zipper@colorado.edu>
2023-09-27 17:13:52 -0600

    reformat code

--- measure_bdt.py
+++ measure_bdt.py
@@ -5,55 +5,74 @@ import numpy as np
 import uproot as ur
 from pathlib import Path
 from joblib import load
-from xgboost import XGBClassifier
+# from xgboost import XGBClassifier
+
 
 def load_dir_args(args):
-    with open(os.path.join(args.fromdir,'log.txt')) as f:
+    with open(os.path.join(args.fromdir, 'log.txt')) as f:
         for l in f:
-            if 'Decay: ' in l: args.decay = l.split('Decay: ', 1)[1].strip()
-            if 'Inputs: ' in l: args.features = ast.literal_eval(l.split('Inputs: ', 1)[1].strip())
-            if 'Saving Model ' in l: args.model = l.split('Saving Model ', 1)[1].strip()
-    
+            if 'Decay: ' in l:
+                args.decay = l.split('Decay: ', 1)[1].strip()
+            if 'Inputs: ' in l:
+                args.features = ast.literal_eval(
+                    l.split('Inputs: ', 1)[1].strip())
+            if 'Saving Model ' in l:
+                args.model = l.split('Saving Model ', 1)[1].strip()
+
     print(f'Measuring {args.decay} Decay')
     print(f'Using Model {args.model}')
     print(f'Using Input Vector {args.features}')
 
+
 def check_rm_files(files=[]):
     for fl in files:
-        if os.path.isfile(fl): os.system('rm '+fl )
+        if os.path.isfile(fl):
+            os.system('rm '+fl)
+
 
 def edit_filename(path, prefix='', suffix=''):
     path = Path(path)
-    return os.path.join(str(path.parent), (prefix+'_' if prefix else '') + str(path.stem)  + ('_'+suffix if suffix else '') + str(path.suffix))
+    return os.path.join(str(path.parent), (prefix+'_' if prefix else '') + str(path.stem) + ('_'+suffix if suffix else '') + str(path.suffix))
+
 
 def evaluate_bdt(bdt, args, bdt_cols, out_cols, selection):
-    modelname = edit_filename(args.filepath, prefix='measurement', suffix=args.label)
+    modelname = edit_filename(
+        args.filepath, prefix='measurement', suffix=args.label)
     check_rm_files([modelname, modelname.replace('.pkl', '.root')])
 
     with ur.open(args.measurefile) as datafile:
-        data_dict = datafile['mytree'].arrays(bdt_cols, cut=selection if selection else None, library='np')
-        out_branches = datafile['mytree'].arrays(out_cols, cut=selection if selection else None, library='np')
+        data_dict = datafile['mytree'].arrays(
+            bdt_cols, cut=selection if selection else None, library='np')
+        out_branches = datafile['mytree'].arrays(
+            out_cols, cut=selection if selection else None, library='np')
         data = np.transpose(np.stack(list(data_dict.values())))
 
     if selection:
         print('Additional Preselection Cuts:')
         for k, val in selection.items():
             print(f'  -{k}: {val}')
-    
-    decisions = np.array([x[1] for x in bdt.predict_proba(data)], dtype=np.float64)
+
+    decisions = np.array([x[1]
+                         for x in bdt.predict_proba(data)], dtype=np.float64)
     out_branches['xgb'] = decisions
 
     with ur.recreate((modelname.split('.pkl')[0] if '.pkl' in modelname else modelname)+'.root') as outfile:
         outfile['mytreefit'] = out_branches
 
+
 if __name__ == '__main__':
     print('Imports Done')
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', dest='model', default='model.pkl', type=str, help='model name or file')
-    parser.add_argument('--measurefile', dest='measurefile', default=None, type=str, help='file with events for measurement')
-    parser.add_argument('--label', dest='label', default='', type=str, help='label for root file')
-    parser.add_argument('--decay', dest='decay', default='kee', type=str, choices=['kmumu','kee'], help='decay type')
-    parser.add_argument('--fromdir', dest='fromdir', default='', type=str, help='load params from designated model directory')
+    parser.add_argument('--model', dest='model',
+                        default='model.pkl', type=str, help='model name or file')
+    parser.add_argument('--measurefile', dest='measurefile', default=None,
+                        type=str, help='file with events for measurement')
+    parser.add_argument('--label', dest='label', default='',
+                        type=str, help='label for root file')
+    parser.add_argument('--decay', dest='decay', default='kee',
+                        type=str, choices=['kmumu', 'kee'], help='decay type')
+    parser.add_argument('--fromdir', dest='fromdir', default='',
+                        type=str, help='load params from designated model directory')
     args, unknown = parser.parse_known_args()
 
     # select input variables
@@ -61,7 +80,8 @@ if __name__ == '__main__':
     # features = ['Bprob','BsLxy','L2iso/L2pt','Kpt/Bmass','Bcos','Kiso/Kpt','LKdz','LKdr','Bpt/Bmass','Passymetry','Kip3d/Kip3dErr']
     # Otto / 'Run 2' Inputs
     # Missing: BBDphi, BTrkdxy2
-    features = ['Bprob','BsLxy','Bcos','L1pt/Bmass','L2pt/Bmass','Kpt/Bmass','LKdz','L1L2dr','LKdr','L1id', 'L2id','L2iso/L2pt','Kiso/Kpt','Passymetry','Kip3d/Kip3dErr']
+    features = ['Bprob', 'BsLxy', 'Bcos', 'L1pt/Bmass', 'L2pt/Bmass', 'Kpt/Bmass', 'LKdz',
+                'L1L2dr', 'LKdr', 'L1id', 'L2id', 'L2iso/L2pt', 'Kiso/Kpt', 'Passymetry', 'Kip3d/Kip3dErr']
 
     # Jay Inputs
     # Missing: ProbeTracks_DCASig[K], ProbeTracks_dzTrg[K], BToKEE_k_svip2d, BToKEE_l1_dxy_sig, BToKEE_l1_dzTrg, BToKEE_l2_dxy_sig, BToKEE_l2_dzTrg, BToKEE_llkDR
@@ -110,7 +130,3 @@ if __name__ == '__main__':
 
     # evaluate model
     evaluate_bdt(bdt, args, features, out_cols, preselection)
-
-
-
-
