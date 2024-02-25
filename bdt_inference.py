ee4dd376dd53e48c9b6a4f3dfad9812202d7e928
Noah Zipper <noah.zipper@colorado.edu>
2024-02-25 09:50:22 -0700

    move to new format

---
+++ bdt_inference.py
@@ -0,0 +1,284 @@
+import os
+import time
+import argparse
+import yaml
+import numpy as np
+import uproot as ur
+import matplotlib.pyplot as plt
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+from sklearn.utils import resample
+from logging import DEBUG, INFO, WARNING, ERROR
+from utils import *
+
+
+def bdt_inference(dataset_params, model_params, output_params, args):
+    debug_n_evts = None
+
+    # configuration for outputs & logging
+    os.makedirs(output_params.output_dir, exist_ok=True)
+    base_filename = os.path.join(output_params.output_dir, 'measurement.root')
+    lgr = R3KLogger(
+        os.path.join(output_params.output_dir, output_params.log_file), 
+        verbose=args.verbose
+    )
+    
+    with open(args.config, 'r') as f:
+        cfg_str = f'cfg_path: {args.config}\n{f.read()}'
+
+    lgr.log(f'Configuration File:\n{cfg_str}')
+
+    # # load data & mc arrays from input files
+    X_mc, cutvars_mc, weights_mc = read_bdt_arrays(
+        dataset_params.rare_file, 
+        dataset_params.tree_name, 
+        model_params.features, 
+        model_params.sample_weights, 
+        model_params.preselection, 
+        (dataset_params.b_mass_branch, dataset_params.ll_mass_branch),
+        n_evts=debug_n_evts
+    )
+
+    X_data, cutvars_data, weights_data = read_bdt_arrays(
+        dataset_params.data_file,
+        dataset_params.tree_name,
+        model_params.features,
+        None, 
+        model_params.preselection, 
+        (dataset_params.b_mass_branch, dataset_params.ll_mass_branch),
+        n_evts=debug_n_evts
+    )
+
+    y_mc = np.ones(X_mc.shape[0])
+    y_data = np.zeros(X_data.shape[0])
+
+    # create array masks for training
+    mask_sig = np.logical_and(
+        cutvars_mc[dataset_params.ll_mass_branch] > 1.05, 
+        cutvars_mc[dataset_params.ll_mass_branch] < 2.45
+    )
+    mask_bkg_lowq2 = np.logical_and(
+        cutvars_data[dataset_params.ll_mass_branch] > 1.05, 
+        cutvars_data[dataset_params.ll_mass_branch] < 2.45
+    )
+    mask_bkg_sideband = np.logical_or(
+        np.logical_and(
+            cutvars_data[dataset_params.b_mass_branch] > 4.8, 
+            cutvars_data[dataset_params.b_mass_branch] < 5.
+        ), 
+        np.logical_and(
+            cutvars_data[dataset_params.b_mass_branch] > 5.4,
+            cutvars_data[dataset_params.b_mass_branch] < 5.6
+        )
+    ) 
+    mask_bkg = np.logical_and(mask_bkg_lowq2, mask_bkg_sideband)
+
+    # load bdt from template file
+    model = load_external_model(model_params.template_file)
+    
+    # set K-fold validation scheme for retraining model
+    skf = StratifiedKFold(n_splits=3)
+
+    # K-fold loop for data measurement
+    event_idxs = np.array([], dtype=np.int64)
+    scores = np.array([], dtype=np.float64)
+    if args.plot:
+        roc = ROCPlotterKFold(skf)
+
+    lgr.log(f'Training Model for Data Measurement ({skf.get_n_splits()}-fold x-val)', just_print=True)
+    start = time.perf_counter()
+    for fold, (train_data, test_data) in enumerate(skf.split(X_data, y_data)):
+        # mix all MC signal events with fold of background data
+        X = np.concatenate((X_mc, X_data[train_data]))
+        y = np.concatenate((y_mc, y_data[train_data]))
+        weights = np.concatenate((weights_mc, weights_data[train_data]))
+
+        # mask all training events (low-q2 + mass sidebands for data)
+        train_mask = np.concatenate((mask_sig, mask_bkg[train_data]))
+
+        # further split events into training and validation sets before fitting
+        split = train_test_split(X[train_mask], y[train_mask], weights[train_mask], test_size=0.05, random_state=271996)
+        X_train, X_val, y_train, y_val, weights_train, weights_val = split
+        eval_set = ((X_train, y_train), (X_val, y_val))
+
+        # fit model
+        model.fit(
+            X_train, 
+            y_train, 
+            sample_weight=weights_train, 
+            eval_set=eval_set, 
+            verbose=2 if args.verbose else 0
+        )
+
+        # predict bdt scores on data events designated for testing in fold
+        scores = np.append(scores, np.array([x[1] for x in model.predict_proba(X_data[test_data])], dtype=np.float64))
+        event_idxs = np.append(event_idxs, np.array(test_data, dtype=np.int64))
+
+        # add line to roc plot for fold
+        if args.plot:
+            roc.add_fold(model, X_val, y_val)
+
+        lgr.log(f'Finished fold {fold+1} of {skf.get_n_splits()}', just_print=True)
+
+    lgr.log(f'Elapsed Training/Inference Time on Data = {round(time.perf_counter() - start)}s', just_print=True)
+
+    # save plots
+    if args.plot:
+        roc.save(os.path.join(output_params.output_dir, 'roc.png'), show=False)
+
+    # save data measurement file
+    output_filename = edit_filename(base_filename, suffix='data')
+    output_branch_names = output_params.output_branches['common'] + output_params.output_branches['common']
+    save_bdt_arrays(
+        dataset_params.data_file, 
+        dataset_params.tree_name, 
+        output_filename, 
+        dataset_params.tree_name, 
+        output_branch_names, 
+        output_params.score_branch, 
+        scores, 
+        event_idxs, 
+        preselection=model_params.preselection, 
+        n_evts=debug_n_evts,
+    )
+    lgr.log(f'Data Measurement File: {output_filename}')
+
+    # TODO down-sample background events
+    downsample = False
+    bkg_sig_ratio = 3
+    if downsample:
+        pass
+
+    # K-fold loop for rare mc measurement
+    event_idxs = np.array([], dtype=np.int64)
+    scores = np.array([], dtype=np.float64)
+    lgr.log(f'Training Model for MC Measurement ({skf.get_n_splits()}-fold x-val)', just_print=True)
+    start = time.perf_counter()
+    for fold, (train_mc, test_mc) in enumerate(skf.split(X_mc, y_mc)):
+        # add all data background events to fold of mc
+        X = np.concatenate((X_mc[train_mc], X_data))
+        y = np.concatenate((y_mc[train_mc], y_data))
+        weights = np.concatenate((weights_mc[train_mc], weights_data))
+
+        # mask all training events (low-q2 + mass sidebands for data)
+        train_mask = np.concatenate((mask_sig[train_mc], mask_bkg))
+
+        # further split events into training and validation sets before fitting
+        X_train, X_val, y_train, y_val, weights_train, weights_val = train_test_split(X[train_mask], y[train_mask], weights[train_mask], test_size=0.05, random_state=271996)
+        eval_set = ((X_train, y_train), (X_val, y_val))
+
+        # fit model
+        model.fit(
+            X_train, 
+            y_train, 
+            sample_weight=weights_train, 
+            eval_set=eval_set, 
+            verbose=2 if args.verbose else 0
+        )
+
+        # predict bdt scores on data events designated for testing in fold
+        scores = np.append(scores, np.array([x[1] for x in model.predict_proba(X_mc[test_mc])], dtype=np.float64))
+        event_idxs = np.append(event_idxs, np.array(test_mc, dtype=np.int64))
+        lgr.log(f'Finished fold {fold+1} of {skf.get_n_splits()}', just_print=True)
+
+    lgr.log(f'Elapsed Training/Inference Time = {round(time.perf_counter() - start)}s', just_print=True)
+    
+    # save rare mc measurement file
+    output_filename = edit_filename(base_filename, suffix='rare')
+    output_branch_names = output_params.output_branches['common'] + output_params.output_branches['mc']
+    save_bdt_arrays(
+        dataset_params.rare_file, 
+        dataset_params.tree_name, 
+        output_filename, 
+        dataset_params.tree_name, 
+        output_branch_names, 
+        output_params.score_branch, 
+        scores, 
+        event_idxs, 
+        preselection=model_params.preselection, 
+        n_evts=debug_n_evts,
+    )
+    lgr.log(f'MC (Rare) Measurement File: {output_filename}')
+
+    if dataset_params.jpsi_file:
+        # load data & mc arrays from input files
+        X_jpsi, cutvars_mc, weights_mc = read_bdt_arrays(
+            dataset_params.jpsi_file, 
+            dataset_params.tree_name, 
+            model_params.features, 
+            model_params.sample_weights, 
+            model_params.preselection, 
+            (dataset_params.b_mass_branch, dataset_params.ll_mass_branch),
+            n_evts=debug_n_evts,
+        )
+
+        # bdt inference
+        scores = np.array([x[1] for x in model.predict_proba(X_jpsi)], dtype=np.float64)
+
+        # save jpsi mc measurement file
+        output_filename = edit_filename(base_filename, suffix='jpsi')
+        output_branch_names = output_params.output_branches['common'] + output_params.output_branches['mc']
+        save_bdt_arrays(
+            dataset_params.jpsi_file, 
+            dataset_params.tree_name, 
+            output_filename, 
+            dataset_params.tree_name, 
+            output_branch_names, 
+            output_params.score_branch, 
+            scores, 
+            None, 
+            preselection=model_params.preselection, 
+            n_evts=debug_n_evts,
+        )
+        lgr.log(f'MC (JPsi) Measurement File: {output_filename}')
+ 
+    if dataset_params.psi2s_file:
+        # load data & mc arrays from input files
+        X_psi2s, cutvars_mc, weights_mc = read_bdt_arrays(
+            dataset_params.psi2s_file, 
+            dataset_params.tree_name, 
+            model_params.features, 
+            model_params.sample_weights, 
+            model_params.preselection, 
+            (dataset_params.b_mass_branch, dataset_params.ll_mass_branch),
+            n_evts=debug_n_evts,
+        )
+
+        # bdt inference
+        scores = np.array([x[1] for x in model.predict_proba(X_psi2s)], dtype=np.float64)
+
+        # save psi2s mc measurement file
+        output_filename = edit_filename(base_filename, suffix='psi2s')
+        output_branch_names = output_params.output_branches['common'] + output_params.output_branches['mc']
+        save_bdt_arrays(
+            dataset_params.psi2s_file, 
+            dataset_params.tree_name, 
+            output_filename, 
+            dataset_params.tree_name, 
+            output_branch_names, 
+            output_params.score_branch, 
+            scores, 
+            None, 
+            preselection=model_params.preselection, 
+            n_evts=debug_n_evts,
+        )
+        lgr.log(f'MC (Psi2S) Measurement File: {output_filename}')
+
+def main(args):
+    with open(args.config, 'r') as f:
+        cfg = yaml.safe_load(f)
+
+    dataset_params = argparse.Namespace(**cfg['datasets'])
+    model_params = argparse.Namespace(**cfg['model'])
+    output_params = argparse.Namespace(**cfg['output'])
+
+    bdt_inference(dataset_params, model_params ,output_params, args)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', dest='config', type=str, required=True, help='BDT configuration file (.yml)')
+    parser.add_argument('-np', '--no_plot', dest='plot', action='store_false', help='dont add loss plot to output directory')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='print parameters and training progress to stdout')
+    args, _ = parser.parse_known_args()
+
+    main(args)
--- utils.py
+++ utils.py
@@ -1,31 +1,172 @@
 import os
+import sys
 import logging
 import ast
+import importlib.util
 import numpy as np
 import multiprocessing as mp
-import matplotlib.pyplot as plt
 from xgboost import Booster
 from joblib import dump,load
 from glob import glob
 from pathlib import Path
 from sklearn.metrics import auc, RocCurveDisplay, PrecisionRecallDisplay
 
+from logging import DEBUG, INFO, WARNING, ERROR
 
-class Logger():
+BACKEND = 'np'
+
+
+class R3KLogger():
     def __init__(self, filepath, verbose=True):        
         self.filepath = filepath
         self.verbose = verbose
-        if os.path.exists(self.filepath):
-            os.remove(self.filepath)
         
-        log_format = '%(levelname)s | %(asctime)s | %(message)s'
-        logging.basicConfig(level=logging.INFO, format=log_format)
+        self.base_logger = logging.getLogger('base_logger')
+        self.base_logger.setLevel(logging.INFO)
+        self.stdout_logger = logging.getLogger('stdout_logger')
+        self.stdout_logger.setLevel(logging.INFO)
+        self.fout_logger = logging.getLogger('fout_logger')
+        self.fout_logger.setLevel(logging.INFO)
+        self.formatter = logging.Formatter('%(levelname)s | %(asctime)s | %(message)s')
 
-    def log(self, string):
-        with open(self.filepath, 'a+') as f:
-            f.write(string+'\n')
-        if self.verbose:
-            logging.info(string)
+        self.fh = logging.FileHandler(self.filepath, mode='w')
+        self.fh.setFormatter(self.formatter)
+        self.fh.setLevel(logging.INFO)
+
+        self.sh = logging.StreamHandler(sys.stdout)
+        self.sh.setFormatter(self.formatter)
+        self.sh.setLevel(logging.INFO)
+
+        self.base_logger.addHandler(self.sh)
+        self.stdout_logger.addHandler(self.sh)
+        self.base_logger.addHandler(self.fh)
+        self.fout_logger.addHandler(self.fh)
+
+
+    def log(self, string, just_print=False, just_write=False, level=INFO):
+        if just_print:
+            if self.verbose:
+                self.stdout_logger.log(level, string)
+        elif just_write:
+            self.fout_logger.log(level, string)
+        else:
+            if self.verbose:
+                self.base_logger.log(level, string)
+            else:
+                self.fout_logger.log(level, string)
+
+
+class ROCPlotterKFold():
+    def __init__(self, kf):
+        self.kf = kf
+        self.ifold = 0
+        self.tprs = []
+        self.aucs = []
+        self.mean_fpr = np.linspace(0, 1, 100)
+        self.fig, self.ax = plt.subplots(figsize=(6, 6))
+
+    def add_fold(self, model, X, y):
+        self.ifold += 1
+        _viz = RocCurveDisplay.from_estimator(
+            model,
+            X,
+            y,
+            name=f'ROC fold {self.ifold}',
+            alpha=0.3,
+            lw=1,
+            ax=self.ax,
+            plot_chance_level=(self.ifold ==self.kf.get_n_splits() - 1),
+        )
+
+        _interp_tpr = np.interp(self.mean_fpr, _viz.fpr, _viz.tpr)
+        _interp_tpr[0] = 0.0
+        self.tprs.append(_interp_tpr)
+        self.aucs.append(_viz.roc_auc)
+
+    def save(self, path, show=False, zoom=False):
+        mean_tpr = np.mean(self.tprs, axis=0)
+        mean_tpr[-1] = 1.0
+        mean_auc = auc(self.mean_fpr, mean_tpr)
+        std_auc = np.std(self.aucs)
+        self.ax.plot(
+            self.mean_fpr,
+            mean_tpr,
+            color='b',
+            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            lw=2,
+            alpha=0.8,
+        )
+
+        std_tpr = np.std(self.tprs, axis=0)
+        tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+        tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+        self.ax.fill_between(
+            self.mean_fpr,
+            tprs_lower,
+            tprs_upper,
+            color='grey',
+            alpha=0.2,
+            label=r'$\pm$ 1 std. dev.',
+        )
+
+        self.ax.set(
+            xlabel='False Positive Rate',
+            ylabel='True Positive Rate',
+            title=f'Mean ROC curve with variability\n(Positive label "Signal")',
+        )
+        self.ax.legend(loc='lower right')
+    
+        if zoom:
+            self.ax.set_xlim([0.,1.])
+            # self.ax.set_xscale('log')
+            self.ax.set_ylim([.9,1.05])
+            # self.ax.set_xscale('log')
+
+        if show:
+            plt.show(block=True)
+
+        plt.savefig(path)
+
+
+def read_bdt_arrays(file, tree, features, weights_branch=None, preselection=None, cutvar_branches=('Bmass', 'Mll'), n_evts=None):
+    all_branches = list(set(features) | set(cutvar_branches))
+    if weights_branch:
+        all_branches += [weights_branch]
+
+    with ur.open(file) as f:
+        all_arrays = f[tree].arrays(all_branches, cut=preselection, entry_stop=n_evts, library=BACKEND)
+
+    features_array = np.stack([all_arrays[k] for k in features]).T
+    cutvars_dict  = {k:all_arrays[k] for k in cutvar_branches}
+    weights_array  = all_arrays[weights_branch] if weights_branch else np.ones(features_array.shape[0])
+
+    return features_array, cutvars_dict, weights_array
+
+def save_bdt_arrays(input_file, input_tree, output_file, output_tree, output_branch_names, score_branch, scores, idxs=None, preselection=None, n_evts=None):    
+    with ur.open(input_file) as f_in:
+        output_branches = f_in[input_tree].arrays(output_branch_names, cut=preselection, entry_stop=n_evts, library=BACKEND)
+
+        if idxs is not None:
+            for br in output_branches.values():
+                br = br[idxs]
+
+        output_branches[score_branch] = scores
+        output_branches['trigger_OR'] = np.ones_like(scores)
+
+        with ur.recreate(output_file, compression=ur.LZMA(9)) as f_out:
+            f_out[output_tree] = output_branches
+
+
+def load_external_model(filepath, model_name='model'):
+    spec = importlib.util.spec_from_file_location('tmp_module', filepath)
+    source_module = importlib.util.module_from_spec(spec)
+    sys.modules['tmp_module'] = source_module
+    spec.loader.exec_module(source_module)
+    model = getattr(source_module,model_name)
+
+    assert callable(getattr(model, 'fit')) and callable(getattr(model, 'predict_proba'))
+
+    return model
 
 
 def make_file_name(args):
@@ -124,154 +265,5 @@ def check_rm_files(files=[]):
 
 def edit_filename(path, prefix='', suffix=''):
     path = Path(path)
-    return os.path.join(str(path.parent), (prefix+'_' if prefix else '') + str(path.stem) + ('_'+suffix if suffix else '') + str(path.suffix))
-
-def logloss_plot(results, args):
-    fig, ax = plt.subplots()
-    train_logloss = results['validation_0']['logloss']
-    test_logloss  = results['validation_1']['logloss']
-    ax.plot(np.arange(len(train_logloss)), train_logloss, label='Train')
-    ax.plot(np.arange(len(test_logloss)), test_logloss, label='Test')
-    ax.legend()
-    plt.xlabel('Epoch')
-    plt.ylabel('Log Loss')
-    plt.title('Log Loss Curve')
-    plt.savefig(os.path.join(args.outdir, f'logloss_{make_file_name(args)}.png'))
-
-def auc_plot(results, args):
-    fig, ax = plt.subplots()
-    train_logloss = results['validation_0']['auc']
-    test_logloss  = results['validation_1']['auc']
-    ax.plot(np.arange(len(train_logloss)), train_logloss, label='Train')
-    ax.plot(np.arange(len(test_logloss)), test_logloss, label='Test')
-    ax.legend()
-    plt.xlabel('Epoch')
-    plt.ylabel('ROC AUC')
-    plt.title('ROC AUC Curve')
-    plt.savefig(os.path.join(args.outdir, f'auc_{make_file_name(args)}.png'))
-    
-def aucpr_plot(results, args):
-    fig, ax = plt.subplots()
-    train_logloss = results['validation_0']['aucpr']
-    test_logloss  = results['validation_1']['aucpr']
-    ax.plot(np.arange(len(train_logloss)), train_logloss, label='Train')
-    ax.plot(np.arange(len(test_logloss)), test_logloss, label='Test')
-    ax.legend()
-    plt.xlabel('Epoch')
-    plt.ylabel('PR AUC')
-    plt.title('Precision-Recall AUC Curve')
-    plt.savefig(os.path.join(args.outdir, f'aucpr_{make_file_name(args)}.png'))
-
-def roc_curve(model, X, y, weights, args, cv):
-    tprs = []
-    aucs = []
-    mean_fpr = np.linspace(0, 1, 100)
-
-    fig, ax = plt.subplots(figsize=(6, 6))
-    for fold, (train, test) in enumerate(cv.split(X, y)):
-        model.fit(X[train], y[train], sample_weight=weights[train])
-        viz = RocCurveDisplay.from_estimator(
-            model,
-            X[test],
-            y[test],
-            name=f'ROC fold {fold}',
-            alpha=0.3,
-            lw=1,
-            ax=ax,
-            plot_chance_level=(fold == cv.get_n_splits() - 1),
-        )
-        interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
-        interp_tpr[0] = 0.0
-        tprs.append(interp_tpr)
-        aucs.append(viz.roc_auc)
-
-    mean_tpr = np.mean(tprs, axis=0)
-    mean_tpr[-1] = 1.0
-    mean_auc = auc(mean_fpr, mean_tpr)
-    std_auc = np.std(aucs)
-    ax.plot(
-        mean_fpr,
-        mean_tpr,
-        color='b',
-        label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-        lw=2,
-        alpha=0.8,
-    )
-
-    std_tpr = np.std(tprs, axis=0)
-    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    ax.fill_between(
-        mean_fpr,
-        tprs_lower,
-        tprs_upper,
-        color='grey',
-        alpha=0.2,
-        label=r'$\pm$ 1 std. dev.',
-    )
-
-    ax.set(
-        xlabel='False Positive Rate',
-        ylabel='True Positive Rate',
-        title=f'Mean ROC curve with variability\n(Positive label "Signal")',
-    )
-    ax.legend(loc='lower right')
-    plt.savefig(os.path.join(args.outdir, f'roc_{make_file_name(args)}.png'))
-
-def pr_curve(model, X, y, weights, args, cv):
-    precisions = []
-    aucs = []
-    mean_recall = np.linspace(0, 1, 100)
-
-    fig, ax = plt.subplots(figsize=(6, 6))
-    for fold, (train, test) in enumerate(cv.split(X, y)):
-        model.fit(X[train], y[train], sample_weight=weights[train])
-
-        viz = PrecisionRecallDisplay.from_estimator(
-            model,
-            X[test],
-            y[test],
-            name=f'PR fold {fold}',
-            alpha=0.3,
-            lw=1,
-            ax=ax,
-            plot_chance_level=(fold == cv.get_n_splits() - 1),
-        )
-
-        interp_precision = np.interp(mean_recall, viz.recall[::-1], viz.precision[::-1])
-        interp_precision[0] = 1.0
-        precisions.append(interp_precision)
-        aucs.append(auc(viz.recall, viz.precision))
-
-    mean_precision = np.mean(precisions, axis=0)
-    # mean_precision[-1] = 0.0
-    mean_auc = auc(mean_recall, mean_precision)
-    std_auc = np.std(aucs)
-    ax.plot(
-        mean_recall,
-        mean_precision,
-        color='b',
-        label=r'Mean PR (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
-        lw=2,
-        alpha=0.8,
-    )
-
-    std_precision = np.std(precisions, axis=0)
-    precisions_upper = np.minimum(mean_precision + std_precision, 1)
-    precisions_lower = np.maximum(mean_precision - std_precision, 0)
-    ax.fill_between(
-        mean_recall,
-        precisions_lower,
-        precisions_upper,
-        color='grey',
-        alpha=0.2,
-        label=r'$\pm$ 1 std. dev.',
-    )
-
-    ax.set(
-        xlabel='Recall',
-        ylabel='Precision',
-        title=f'Mean PR curve with variability\n(Positive label "Signal")',
-    )
-    ax.legend(loc='center left')
-    plt.savefig(os.path.join(args.outdir, f'pr_{make_file_name(args)}.png'))
+    path = path.with_stem('_'.join(filter(None, [prefix, str(path.stem), suffix])))
+    return path
