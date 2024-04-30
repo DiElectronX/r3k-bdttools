5e55cf6f4b99881c4c5dc13eac7e746f5b2aa385
Noah Zipper <noah.zipper@colorado.edu>
2024-04-30 09:51:34 -0600

    memory optimizations for handling large input files

--- bdt_inference.py
+++ bdt_inference.py
@@ -1,16 +1,19 @@
 import os
+from pathlib import Path
+import shutil
 import time
 import argparse
 import yaml
 import numpy as np
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from logging import DEBUG, INFO, WARNING, ERROR
-from utils import R3KLogger, ROCPlotterKFold, read_bdt_arrays, save_bdt_arrays, load_external_model, edit_filename, get_branches
+from utils import R3KLogger, ROCPlotterKFold, read_bdt_arrays, \
+    save_bdt_arrays, load_external_model, edit_filename, get_branches
 
+import gc
 
 def bdt_inference(dataset_params, model_params, output_params, args):
     debug_n_evts = 10000 if args.debug else None
-    args.verbose = True if args.debug else False
 
     # configuration for outputs & logging
     os.makedirs(output_params.output_dir, exist_ok=True)
@@ -25,7 +28,7 @@ def bdt_inference(dataset_params, model_params, output_params, args):
 
     lgr.log(f'Configuration File:\n{cfg_str}')
 
-    # # load data & mc arrays from input files
+    # load data & mc arrays from input files
     X_mc, cutvars_mc, weights_mc = read_bdt_arrays(
         dataset_params.rare_file, 
         dataset_params.tree_name, 
@@ -85,16 +88,16 @@ def bdt_inference(dataset_params, model_params, output_params, args):
     lgr.log(f'Training Model for Data Measurement ({skf.get_n_splits()}-fold x-val)', just_print=True)
     start = time.perf_counter()
     for fold, (train_data, test_data) in enumerate(skf.split(X_data, y_data)):
-        # mix all MC signal events with fold of background data
-        X = np.concatenate((X_mc, X_data[train_data]))
-        y = np.concatenate((y_mc, y_data[train_data]))
-        weights = np.concatenate((weights_mc, weights_data[train_data]))
-
         # mask all training events (low-q2 + mass sidebands for data)
         train_mask = np.concatenate((mask_sig, mask_bkg[train_data]))
 
-        # further split events into training and validation sets before fitting
-        split = train_test_split(X[train_mask], y[train_mask], weights[train_mask], test_size=0.05, random_state=271996)
+        # split events into training and validation sets from mc and data fold
+        split = train_test_split(
+            np.concatenate((X_mc, X_data[train_data]))[train_mask], 
+            np.concatenate((y_mc, y_data[train_data]))[train_mask], 
+            np.concatenate((weights_mc, weights_data[train_data]))[train_mask],
+            test_size=0.05, random_state=271996
+        )
         X_train, X_val, y_train, y_val, weights_train, weights_val = split
         eval_set = ((X_train, y_train), (X_val, y_val))
 
@@ -108,7 +111,7 @@ def bdt_inference(dataset_params, model_params, output_params, args):
         )
 
         # predict bdt scores on data events designated for testing in fold
-        scores = np.append(scores, np.array([x[1] for x in model.predict_proba(X_data[test_data])], dtype=np.float64))
+        scores = np.append(scores, model.predict_proba(X_data[test_data])[:,0].astype(np.float64))
         event_idxs = np.append(event_idxs, np.array(test_data, dtype=np.int64))
 
         # add line to roc plot for fold
@@ -116,6 +119,9 @@ def bdt_inference(dataset_params, model_params, output_params, args):
             roc.add_fold(model, X_val, y_val)
 
         lgr.log(f'Finished fold {fold+1} of {skf.get_n_splits()}', just_print=True)
+        del split, train_mask, X_train, X_val, y_train, y_val, weights_train, \
+            weights_val, eval_set, train_data, test_data
+        gc.collect()
 
     lgr.log(f'Elapsed Training/Inference Time on Data = {round(time.perf_counter() - start)}s', just_print=True)
 
@@ -139,44 +145,13 @@ def bdt_inference(dataset_params, model_params, output_params, args):
         n_evts=debug_n_evts,
     )
     lgr.log(f'Data Measurement File: {output_filename}')
+    del scores, event_idxs
+    gc.collect()
 
-    # predict bdt scores for other data files in config
-    if dataset_params.other_data_files:
-        for data_name, data_file in dataset_params.other_data_files.items():
-            # load arrays from input file
-            X_data_extra, _, _ = read_bdt_arrays(
-                data_file, 
-                dataset_params.tree_name, 
-                model_params.features, 
-                None, 
-                model_params.preselection, 
-                (dataset_params.b_mass_branch, dataset_params.ll_mass_branch),
-                n_evts=debug_n_evts,
-            )
-
-            # bdt inference
-            scores = np.array([x[1] for x in model.predict_proba(X_data_extra)], dtype=np.float64)
-
-            # save jpsi mc measurement file
-            output_filename = edit_filename(base_filename, suffix=data_name)
-            output_branch_names = get_branches(output_params, ['common','data'])
-            save_bdt_arrays(
-                data_file, 
-                dataset_params.tree_name, 
-                output_filename, 
-                dataset_params.tree_name, 
-                output_branch_names, 
-                output_params.score_branch, 
-                scores, 
-                preselection=model_params.preselection, 
-                n_evts=debug_n_evts,
-            )
-            lgr.log(f'Data ({data_name}) Measurement File: {output_filename}')
-
-    # TODO down-sample background events
-    downsample = False
-    if downsample:
-        pass
+    # # TODO down-sample background events
+    # downsample = False
+    # if downsample:
+    #     pass
 
     # K-fold loop for rare mc measurement
     event_idxs = np.array([], dtype=np.int64)
@@ -184,16 +159,17 @@ def bdt_inference(dataset_params, model_params, output_params, args):
     lgr.log(f'Training Model for MC Measurement ({skf.get_n_splits()}-fold x-val)', just_print=True)
     start = time.perf_counter()
     for fold, (train_mc, test_mc) in enumerate(skf.split(X_mc, y_mc)):
-        # add all data background events to fold of mc
-        X = np.concatenate((X_mc[train_mc], X_data))
-        y = np.concatenate((y_mc[train_mc], y_data))
-        weights = np.concatenate((weights_mc[train_mc], weights_data))
-
         # mask all training events (low-q2 + mass sidebands for data)
         train_mask = np.concatenate((mask_sig[train_mc], mask_bkg))
 
-        # further split events into training and validation sets before fitting
-        X_train, X_val, y_train, y_val, weights_train, weights_val = train_test_split(X[train_mask], y[train_mask], weights[train_mask], test_size=0.05, random_state=271996)
+        # split events into training and validation sets from mc fold and data
+        split = train_test_split(
+            np.concatenate((X_mc[train_mc], X_data))[train_mask], 
+            np.concatenate((y_mc[train_mc], y_data))[train_mask], 
+            np.concatenate((weights_mc[train_mc], weights_data))[train_mask], 
+            test_size=0.05, random_state=271996
+        )
+        X_train, X_val, y_train, y_val, weights_train, weights_val = split
         eval_set = ((X_train, y_train), (X_val, y_val))
 
         # fit model
@@ -206,9 +182,13 @@ def bdt_inference(dataset_params, model_params, output_params, args):
         )
 
         # predict bdt scores on data events designated for testing in fold
-        scores = np.append(scores, np.array([x[1] for x in model.predict_proba(X_mc[test_mc])], dtype=np.float64))
+        scores = np.append(scores, model.predict_proba(X_mc[test_mc])[:,0].astype(np.float64))
         event_idxs = np.append(event_idxs, np.array(test_mc, dtype=np.int64))
+
         lgr.log(f'Finished fold {fold+1} of {skf.get_n_splits()}', just_print=True)
+        del split, train_mask, X_train, X_val, y_train, y_val, weights_train, \
+            weights_val, eval_set, train_mc, test_mc
+        gc.collect()
 
     lgr.log(f'Elapsed Training/Inference Time = {round(time.perf_counter() - start)}s', just_print=True)
     
@@ -228,10 +208,52 @@ def bdt_inference(dataset_params, model_params, output_params, args):
         n_evts=debug_n_evts,
     )
     lgr.log(f'MC (Rare) Measurement File: {output_filename}')
+    del scores, event_idxs
+    gc.collect()
+
+    # Delete arrays needed for training, save memory
+    del X_mc, cutvars_mc, weights_mc, X_data, cutvars_data, weights_data, \
+        y_mc, y_data, mask_sig, mask_bkg_lowq2, mask_bkg_sideband
+    gc.collect()
 
     # predict bdt scores for other data files in config
+    if dataset_params.other_data_files:
+        for data_name, data_file in dataset_params.other_data_files.items():
+            # load arrays from input file
+            X_data_extra, _, _ = read_bdt_arrays(
+                data_file, 
+                dataset_params.tree_name, 
+                model_params.features, 
+                None, 
+                model_params.preselection, 
+                (dataset_params.b_mass_branch, dataset_params.ll_mass_branch),
+                n_evts=debug_n_evts,
+            )
+
+            # bdt inference
+            scores = model.predict_proba(X_data_extra)[:,0].astype(np.float64)
+
+            # save jpsi mc measurement file
+            output_filename = edit_filename(base_filename, suffix=data_name)
+            output_branch_names = get_branches(output_params, ['common','data'])
+            save_bdt_arrays(
+                data_file, 
+                dataset_params.tree_name, 
+                output_filename, 
+                dataset_params.tree_name, 
+                output_branch_names, 
+                output_params.score_branch, 
+                scores, 
+                preselection=model_params.preselection, 
+                n_evts=debug_n_evts,
+            )
+            lgr.log(f'Data ({data_name}) Measurement File: {output_filename}')
+            del X_data_extra, scores
+            gc.collect()
+
+    # predict bdt scores for other mc files in config
     if dataset_params.other_mc_files:
-        for mc_name, mc_file in dataset_params.other_mc_files.items():        # load data & mc arrays from input files
+        for mc_name, mc_file in dataset_params.other_mc_files.items():
             X_mc_extra, _, _ = read_bdt_arrays(
                 mc_file, 
                 dataset_params.tree_name, 
@@ -243,7 +265,7 @@ def bdt_inference(dataset_params, model_params, output_params, args):
             )
 
             # bdt inference
-            scores = np.array([x[1] for x in model.predict_proba(X_mc_extra)], dtype=np.float64)
+            scores = model.predict_proba(X_mc_extra)[:,0].astype(np.float64)
 
             # save jpsi mc measurement file
             output_filename = edit_filename(base_filename, suffix=mc_name)
@@ -260,7 +282,9 @@ def bdt_inference(dataset_params, model_params, output_params, args):
                 n_evts=debug_n_evts,
             )
             lgr.log(f'MC ({mc_name}) Measurement File: {output_filename}')
- 
+            del X_mc_extra, scores
+            gc.collect()
+
 
 def main(args):
     with open(args.config, 'r') as f:
@@ -270,7 +294,15 @@ def main(args):
     model_params = argparse.Namespace(**cfg['model'])
     output_params = argparse.Namespace(**cfg['output'])
 
+    if args.debug:
+        args.verbose = True
+        output_params.output_dir = Path('outputs/tmp')
+
     bdt_inference(dataset_params, model_params ,output_params, args)
+
+    if args.debug:
+        if output_params.output_dir.exists() and output_params.output_dir.is_dir():
+            shutil.rmtree(output_params.output_dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
